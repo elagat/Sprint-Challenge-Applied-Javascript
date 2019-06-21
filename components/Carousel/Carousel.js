6afc10c308464aba7e48b8a27f27f3516328f2c2
Emily Lagattolla <elagattolla@gmail.com>
2019-06-21 12:27:04 -0500

    Stretch: wrote skeleton for carousel constructor function. WIP: changeImage()

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,28 @@
 class Carousel {
+  constructor(carousel) {
+    // 1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
+    this.carousel = carousel;
+    this.leftButton = this.carousel.querySelector('.leftButton');
+    this.rightButton = this.carousel.querySelector('.rightButton');
 
+    // 2. You will need to grab a reference to all of the images
+    this.images = this.carousel.getElementsByTagName('img');
+
+    // 3. Create a current index
+    this.images = Array.from(this.images);
+
+    // 4. Those buttons are gonna need some click handlers.
+    this.leftButton.addEventListener('click', changeImage(i++));
+    this.rightButton.addEventListener('click', changeImage(i--));
+
+  }
+
+  changeImage(direction) {
+    
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
